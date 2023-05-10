b2daeea7f0c8b19ebff51728fbfef609cd12ef73
Kirill Kiselev <sakuradezkol@gmail.com>
2023-05-10 16:41:02 +0400

    Сортировка таблицы по столбцам(доделать сортировку по string)

--- EquipmentMonitoringSystem/wwwroot/js/site.js
+++ EquipmentMonitoringSystem/wwwroot/js/site.js
@@ -1,5 +1,4 @@
 ﻿
-
 (function ($) {
     /*Preloader*/
     $(window).on('load', function () {
@@ -76,3 +75,88 @@ jQueryAjaxDelete = form => {
     //prevent default form submit event
     return false;
 }
+
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("info-table");
+    switching = true;
+
+    while (switching) {
+
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+
+        for (i = 1; i < (rows.length - 1); i++) {
+
+            shouldSwitch = false;
+
+            x = rows[i].getElementsByTagName("TD")[0];
+            y = rows[i + 1].getElementsByTagName("TD")[0];
+
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+function tableSearch() {
+    var phrase = document.getElementById('search-text');
+    var table = document.getElementById('info-table');
+    var regPhrase = new RegExp(phrase.value, 'i');
+    var flag = false;
+    for (var i = 1; i < table.rows.length; i++) {
+        flag = false;
+        for (var j = table.rows[i].cells.length - 1; j >= 0; j--) {
+            flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
+            if (flag) break;
+        }
+        if (flag) {
+            table.rows[i].style.display = "";
+        } else {
+            table.rows[i].style.display = "none";
+        }
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    var table = document.getElementById('info-table');
+    table.onclick = function (e) {
+        if (e.target.tagName != 'TH') return;
+        sortTable(e.target.cellIndex, e.target.getAttribute('data-type'));
+    };
+    function sortTable(colNum, type) {
+        var tbody = table.getElementsByTagName('tbody')[0];
+        var rowsArray = [].slice.call(tbody.rows);
+        var compare;
+        switch (type) {
+            case 'number':
+                compare = function (rowA, rowB) {
+                    return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+                };
+                break;
+            case 'string':
+                compare = function (rowA, rowB) {
+                    return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML;
+                };
+                break;
+        }
+        rowsArray.sort(compare);
+        table.removeChild(tbody);
+        for (var i = 0; i < rowsArray.length; i++) {
+            tbody.appendChild(rowsArray[i]);
+        }
+        table.appendChild(tbody);
+
+    }
+
+});
+
